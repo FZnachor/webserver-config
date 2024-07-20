fb55a9f9f70cbfba0d5ac13c7c2e080af8fe505b
Filip Znachor <filip@znachor.cz>
2024-07-20 21:23:59 +0200

    Improved apache2 config builder

--- src/apache2.rs
+++ src/apache2.rs
@@ -1,3 +1,5 @@
+use std::borrow::BorrowMut;
+
 impl Config {
 
 	pub fn new() -> Config {
@@ -5,16 +7,20 @@ impl Config {
 	}
 
 	pub fn add_directive(self: &mut Config, directive: Directive) {
-		self.content.push(directive);
+		self.content.push(ConfigItem::Directive(directive));
+	}
+
+	pub fn add_block(self: &mut Config, block: Block) {
+		self.content.push(ConfigItem::Block(block));
 	}
 
 	pub fn add(self: &mut Config, name: &str, value: &str) {
-		self.content.push(Directive {name: name.to_string(), values: vec![value.to_string()]});
+		self.add_directive(Directive {name: name.to_string(), values: vec![value.to_string()]});
 	}
 
 	pub fn add_multi(self: &mut Config, name: &str, value: Vec<&str>) {
 		let values: Vec<String> = value.iter().map(|&s| s.to_string()).collect();
-		self.content.push(Directive {name: name.to_string(), values});
+		self.add_directive(Directive {name: name.to_string(), values});
 	}
 
 	pub fn to_string(self: Config) -> String {
@@ -24,8 +30,15 @@ impl Config {
 	pub fn to_string_level(self: Config, level: usize) -> String {
 		let mut s = String::new();
 		let padding = " ".repeat(4 * level);
-		for directive in self.content {
-			s.push_str(&format!("{}{} {}\n", padding, directive.name, directive.values.join(" ")));
+		for item in self.content {
+			match item {
+				ConfigItem::Directive(d) => {
+					s.push_str(&format!("{}{} {}\n", padding, d.name, d.values.join(" ")))
+				},
+				ConfigItem::Block(b) => {
+					s.push_str(&b.to_string_level(level))
+				}
+			}
 		}
 		return s;
 	}
@@ -33,15 +46,29 @@ impl Config {
 }
 
 pub struct Config {
-	content: Vec<Directive>
+	content: Vec<ConfigItem>
+}
+
+enum ConfigItem {
+    Directive(Directive),
+    Block(Block)
 }
 
 impl Block {
+
+	pub fn new(name: &str, params: Vec<&str>) -> Block {
+		let conf = Config::new();
+		Block::from(name, params, conf)
+	}
 
 	pub fn from(name: &str, params: Vec<&str>, conf: Config) -> Block {
 		let params: Vec<String> = params.iter().map(|&s| s.to_string()).collect();
 		Block {name: name.to_string(), params, config: conf}
 	}
+
+    pub fn modify_config<F>(&mut self, mut f: F) where F: FnMut(&mut Config) {
+        f(self.config.borrow_mut())
+    }
 
 	pub fn to_string(self: Block) -> String {
 		self.to_string_level(0)
